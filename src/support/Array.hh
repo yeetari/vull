b747032b61fd05eea877c63e0e7c7358f0176e89
Owen Smith <yeeetari@gmail.com>
2020-12-24 21:19:51 +0000

    support: Array and Span improvements
    * Allow Span to be constructed from two iterators
    * Allow Span to be iterated
    * Implement Array::operator[]
    * Implement Span::operator[]

--- src/support/Array.hh
+++ src/support/Array.hh
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <support/Assert.hh>
+
 #include <cstdint>
 
 template <typename T, std::uint32_t N>
 struct Array {
     // NOLINTNEXTLINE
     T m_data[N];
+
+    T &operator[](std::uint32_t index);
+    const T &operator[](std::uint32_t index) const;
 
     T *data() { return static_cast<T *>(m_data); }
     const T *data() const { return static_cast<const T *>(m_data); }
@@ -16,3 +21,15 @@ struct Array {
 
 template <typename T, typename... Args>
 Array(T, Args...) -> Array<T, sizeof...(Args) + 1>;
+
+template <typename T, std::uint32_t N>
+T &Array<T, N>::operator[](std::uint32_t index) {
+    ASSERT(index < N);
+    return m_data[index];
+}
+
+template <typename T, std::uint32_t N>
+const T &Array<T, N>::operator[](std::uint32_t index) const {
+    ASSERT(index < N);
+    return m_data[index];
+}
--- src/support/Span.hh
+++ src/support/Span.hh
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <support/Array.hh>
+#include <support/Assert.hh>
 
 #include <cstdint>
+#include <memory>
 
 /// Dynamically sized span.
 template <typename T, typename SizeType = std::uint32_t>
@@ -13,6 +15,8 @@ class Span {
 public:
     template <SizeType N>
     constexpr Span(Array<T, N> &array) : m_data(array.data()), m_length(array.length()) {}
+    template <typename BeginIt, typename EndIt>
+    constexpr Span(BeginIt begin, EndIt end) : m_data(std::to_address(begin)), m_length(end - begin) {}
     constexpr Span(T *data, SizeType length) : m_data(data), m_length(length) {}
     constexpr ~Span() = default;
 
@@ -24,8 +28,28 @@ public:
     Span &operator=(const Span &) = delete;
     Span &operator=(Span &&) = delete;
 
+    T *begin() { return m_data; }
+    T *end() { return m_data + m_length; }
+    const T *begin() const { return m_data; }
+    const T *end() const { return m_data + m_length; }
+
+    T &operator[](std::uint32_t index);
+    const T &operator[](std::uint32_t index) const;
+
     T *data() { return m_data; }
     const T *data() const { return m_data; }
     SizeType length() const { return m_length; }
     SizeType size() const { return m_length * sizeof(T); }
 };
+
+template <typename T, typename SizeType>
+T &Span<T, SizeType>::operator[](std::uint32_t index) {
+    ASSERT(index < m_length);
+    return m_data[index];
+}
+
+template <typename T, typename SizeType>
+const T &Span<T, SizeType>::operator[](std::uint32_t index) const {
+    ASSERT(index < m_length);
+    return m_data[index];
+}
