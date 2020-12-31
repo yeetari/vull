3c6844060e9dd2badac6c23a1a4e9922c207ed88
Owen Smith <yeeetari@gmail.com>
2020-12-31 22:41:26 +0000

    engine: Add ECS benchmarks

---
+++ engine/benchmarks/EntitySystemBenchmarks.cc
@@ -0,0 +1,87 @@
+#include <vull/core/World.hh>
+
+#include <benchmark/benchmark.h>
+
+namespace {
+
+constexpr float DELTA_TIME = 1.0F / 60.0F;
+
+struct Position {
+    float x;
+    float y;
+    Position(float x, float y) : x(x), y(y) {}
+};
+
+struct Velocity {
+    float x;
+    float y;
+    Velocity(float x, float y) : x(x), y(y) {}
+};
+
+struct PhysicsSystem : public System<PhysicsSystem> {
+    void update(World *world, float dt) override {
+        for (auto entity : world->view<Position, Velocity>()) {
+            auto *position = entity.get<Position>();
+            auto *velocity = entity.get<Velocity>();
+            position->x += velocity->x * dt;
+            position->y += velocity->y * dt;
+        }
+    }
+};
+
+void create_entities(benchmark::State &state) {
+    for (auto _ : state) {
+        World world;
+        for (auto i = 0; i < state.range(); i++) {
+            benchmark::DoNotOptimize(world.create_entity());
+        }
+    }
+}
+
+void iterate_one_component(benchmark::State &state) {
+    World world;
+    for (auto i = 0; i < state.range(); i++) {
+        auto entity = world.create_entity();
+        entity.add<Position>(2, 4);
+    }
+    for (auto _ : state) {
+        for (auto entity : world.view<Position>()) {
+            benchmark::DoNotOptimize(entity.get<Position>());
+        }
+    }
+}
+
+void iterate_two_components(benchmark::State &state) {
+    World world;
+    for (auto i = 0; i < state.range(); i++) {
+        auto entity = world.create_entity();
+        entity.add<Position>(2, 4);
+        entity.add<Velocity>(4, 6);
+    }
+    for (auto _ : state) {
+        for (auto entity : world.view<Position, Velocity>()) {
+            benchmark::DoNotOptimize(entity.get<Position>());
+            benchmark::DoNotOptimize(entity.get<Velocity>());
+        }
+    }
+}
+
+void update_systems(benchmark::State &state) {
+    World world;
+    world.add<PhysicsSystem>();
+    for (auto i = 0; i < state.range(); i++) {
+        auto entity = world.create_entity();
+        entity.add<Position>(2, 4);
+        entity.add<Velocity>(4, 6);
+    }
+    for (auto _ : state) {
+        world.update(DELTA_TIME);
+    }
+}
+
+BENCHMARK(create_entities)->Arg(100000)->Arg(1000000)->Arg(10000000)->Unit(benchmark::TimeUnit::kMillisecond);
+BENCHMARK(iterate_one_component)->Arg(100000)->Arg(1000000)->Arg(10000000)->Unit(benchmark::TimeUnit::kMillisecond);
+BENCHMARK(iterate_two_components)->Arg(100000)->Arg(1000000)->Arg(10000000)->Unit(benchmark::TimeUnit::kMillisecond);
+BENCHMARK(update_systems)->Arg(100000)->Arg(1000000)->Arg(10000000)->Unit(benchmark::TimeUnit::kMillisecond);
+
+} // namespace
