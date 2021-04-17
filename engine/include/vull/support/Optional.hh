0cd84b032c4bbbff86aec73f8abb9046dcde79af
Owen Smith <yeeetari@gmail.com>
2021-04-17 14:01:20 +0100

    support: Create Optional<T> container

---
+++ engine/include/vull/support/Optional.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vull/support/Array.hh>
+#include <vull/support/Assert.hh>
+
+#include <cstdint>
+#include <utility>
+
+template <typename T>
+class Optional {
+    Array<std::uint8_t, sizeof(T)> m_data{};
+    bool m_present{false};
+
+public:
+    constexpr Optional() = default;
+    constexpr Optional(const T &value) : m_present(true) { new (m_data.data()) T(value); }
+    constexpr Optional(T &&value) : m_present(true) { new (m_data.data()) T(std::move(value)); }
+
+    constexpr explicit operator bool() const noexcept { return m_present; }
+    constexpr const T &operator*() const {
+        ASSERT(m_present);
+        return *reinterpret_cast<const T *>(m_data.data());
+    }
+};
