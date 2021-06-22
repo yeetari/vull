99b1471ecb4193345379933764af8e33871c562a
Owen Smith <yeeetari@gmail.com>
2021-06-22 17:06:18 +0100

    sandbox: Update asset pack and assign materials to each object

--- sandbox/main.cc
+++ sandbox/main.cc
@@ -16,6 +16,7 @@
 #include <vull/renderer/Camera.hh>
 #include <vull/renderer/Device.hh>
 #include <vull/renderer/Instance.hh>
+#include <vull/renderer/Material.hh>
 #include <vull/renderer/Mesh.hh>
 #include <vull/renderer/PointLight.hh>
 #include <vull/renderer/RenderSystem.hh>
@@ -94,10 +95,15 @@ int main(int, char **argv) {
     world.add<VehicleControllerSystem>(window);
     world.add<RenderSystem>(device, swapchain, vertices, indices);
 
+    auto *renderer = world.get<RenderSystem>();
     auto cube_mesh = FileSystem::load_mesh("sandbox/meshes/cube");
     auto tire_mesh = FileSystem::load_mesh("sandbox/meshes/tire");
+    auto pink_texture = renderer->upload_texture(FileSystem::load_texture("sandbox/textures/pink_checkerboard"));
+    auto green_texture = renderer->upload_texture(FileSystem::load_texture("sandbox/textures/green_checkerboard"));
+    auto marble_texture = renderer->upload_texture(FileSystem::load_texture("sandbox/textures/marble"));
 
     auto sponza = world.create_entity();
+    sponza.add<Material>(green_texture);
     sponza.add<Mesh>(FileSystem::load_mesh("sandbox/meshes/sponza"));
     sponza.add<Transform>(glm::vec3(0.0f, -24.5f, 300.0f), glm::vec3(0.01f));
 
@@ -105,10 +111,10 @@ int main(int, char **argv) {
     auto floor = world.create_entity();
     floor.add<Collider>(floor_shape);
     floor.add<RigidBody>(floor_shape, 0.0f, 0.0f);
+    floor.add<Material>(marble_texture);
     floor.add<Mesh>(cube_mesh);
     floor.add<Transform>(glm::vec3(0.0f, -25.0f, 0.0f), floor_shape.half_size());
 
-    auto *renderer = world.get<RenderSystem>();
     auto &lights = renderer->lights();
     lights.resize(3000);
     Array<glm::vec3, 3000> dsts{};
@@ -148,6 +154,7 @@ int main(int, char **argv) {
     BoxShape car_shape(glm::vec3(3.0f, 1.0f, 5.0f));
     auto car = world.create_entity();
     car.add<Collider>(car_shape);
+    car.add<Material>(pink_texture);
     car.add<Mesh>(cube_mesh);
     car.add<RigidBody>(car_shape, 5.0_t, 0.1f);
     car.add<Transform>(glm::vec3(100.0f, -15.0f, 95.0f), car_shape.half_size());
@@ -160,6 +167,7 @@ int main(int, char **argv) {
     auto *vehicle = car.add<Vehicle>();
     auto create_wheel = [&](Axle &axle, float radius, float x_offset, float roll) {
         auto visual_wheel = world.create_entity();
+        visual_wheel.add<Material>(pink_texture);
         visual_wheel.add<Mesh>(tire_mesh);
         visual_wheel.add<Transform>(glm::vec3(0.0f));
         axle.add_wheel(radius, x_offset, visual_wheel.id()).set_roll(roll);
@@ -177,6 +185,7 @@ int main(int, char **argv) {
             for (int y = 0; y < 8; y++) {
                 auto box = world.create_entity();
                 box.add<Collider>(box_shape);
+                box.add<Material>(pink_texture);
                 box.add<Mesh>(cube_mesh);
                 box.add<RigidBody>(box_shape, 10.0_kg, 0.0f);
                 box.add<Transform>(glm::vec3(static_cast<float>(x) * 5.0f, static_cast<float>(y) * 4.5f - 15.0f,
@@ -196,6 +205,7 @@ int main(int, char **argv) {
         auto &shape = stair_shapes[i];
         auto stair = world.create_entity();
         stair.add<Collider>(shape);
+        stair.add<Material>(green_texture);
         stair.add<Mesh>(cube_mesh);
         stair.add<RigidBody>(shape, 0.0f, 0.0f);
         stair.add<Transform>(
@@ -206,6 +216,7 @@ int main(int, char **argv) {
     BoxShape ramp_shape(glm::vec3(10.0f, 1.0f, 50.0f));
     auto ramp = world.create_entity();
     ramp.add<Collider>(ramp_shape);
+    ramp.add<Material>(green_texture);
     ramp.add<Mesh>(cube_mesh);
     ramp.add<RigidBody>(ramp_shape, 0.0f, 0.0f);
     ramp.add<Transform>(glm::vec3(100.0f, -12.5f, 150.0f), ramp_shape.half_size())->orientation() =
@@ -215,6 +226,7 @@ int main(int, char **argv) {
     for (int i = 0; i < 10; i++) {
         auto line = world.create_entity();
         line.add<Collider>(line_shape);
+        line.add<Material>(green_texture);
         line.add<Mesh>(cube_mesh);
         line.add<RigidBody>(line_shape, 0.0f, 0.0f);
         line.add<Transform>(glm::vec3(50.0f, -23.4f, static_cast<float>(i) * 8.0f + 200.0f), line_shape.half_size());
@@ -222,6 +234,7 @@ int main(int, char **argv) {
     for (int i = 0; i < 10; i++) {
         auto line = world.create_entity();
         line.add<Collider>(line_shape);
+        line.add<Material>(green_texture);
         line.add<Mesh>(cube_mesh);
         line.add<RigidBody>(line_shape, 0.0f, 0.0f);
         line.add<Transform>(glm::vec3(60.0f, -23.4f, static_cast<float>(i) * 8.0f + 204.0f), line_shape.half_size());
@@ -314,6 +327,7 @@ int main(int, char **argv) {
             user_ptr.fire = false;
             auto bullet = world.create_entity();
             bullet.add<Collider>(sphere_shape);
+            bullet.add<Material>(green_texture);
             bullet.add<Mesh>(FileSystem::load_mesh("sandbox/meshes/suzanne"));
             bullet.add<RigidBody>(sphere_shape, 100.0f, 0.1f);
             bullet.add<Transform>(user_ptr.free_camera_active ? ubo.camera_position : car_position);
@@ -332,4 +346,5 @@ int main(int, char **argv) {
         world.update(dt);
         Window::poll_events();
     }
+    FileSystem::deinitialise();
 }
