1119885b86362e64e858d0620c2f67d7eccea0dd
Owen Smith <yeeetari@gmail.com>
2022-01-13 20:30:59 +0000

    sandbox: Create a vulkan context

--- sandbox/main.cc
+++ sandbox/main.cc
@@ -1,7 +1,9 @@
 #include <vull/core/Window.hh>
+#include <vull/vulkan/Context.hh>
 
 int main() {
     vull::Window window(800, 600);
+    vull::Context context;
     while (!window.should_close()) {
         window.poll_events();
     }
