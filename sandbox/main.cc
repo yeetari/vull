92634caf25a893fde2776f02d59f2334dc13b2b3
Owen Smith <yeeetari@gmail.com>
2022-01-14 21:47:34 +0000

    sandbox: Create a swapchain

--- sandbox/main.cc
+++ sandbox/main.cc
@@ -4,6 +4,7 @@
 int main() {
     vull::Window window(800, 600);
     vull::Context context;
+    auto swapchain = window.create_swapchain(context);
     while (!window.should_close()) {
         window.poll_events();
     }
