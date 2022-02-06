b3b3b2ea1588c7a29a685d850cb8ef2a645a102a
Owen Smith <yeeetari@gmail.com>
2022-02-06 10:27:04 +0000

    core: Window keyboard and mouse event handling

--- engine/include/vull/core/Window.hh
+++ engine/include/vull/core/Window.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vull/support/Array.hh>
 #include <vull/vulkan/Swapchain.hh>
 
 #include <stdint.h>
@@ -10,16 +11,33 @@ namespace vull {
 
 class Context;
 
+enum class Key : uint8_t {
+    Unknown = 0,
+    // clang-format off
+    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+    // clang-format on
+    Shift,
+    Count,
+};
+
 class Window {
-    const uint32_t m_width;
-    const uint32_t m_height;
+    const uint16_t m_width;
+    const uint16_t m_height;
     xcb_connection_t *m_connection;
     xcb_intern_atom_reply_t *m_delete_window_atom{nullptr};
     uint32_t m_id{0};
+
+    int16_t m_delta_x{0};
+    int16_t m_delta_y{0};
+    Array<Key, 256> m_keycode_map{};
+    Array<bool, static_cast<uint8_t>(Key::Count)> m_keys{};
     bool m_should_close{false};
 
+    void map_keycodes();
+    Key translate_keycode(uint8_t keycode);
+
 public:
-    Window(uint32_t width, uint32_t height, bool fullscreen);
+    Window(uint16_t width, uint16_t height, bool fullscreen);
     Window(const Window &) = delete;
     Window(Window &&) = delete;
     ~Window();
@@ -31,6 +49,11 @@ public:
     void close();
     void poll_events();
 
+    bool is_key_down(Key key) const { return m_keys[static_cast<uint8_t>(key)]; }
+    float delta_x() const { return static_cast<float>(m_delta_x); }
+    float delta_y() const { return static_cast<float>(m_delta_y); }
+
+    float aspect_ratio() const { return static_cast<float>(m_width) / static_cast<float>(m_height); }
     uint32_t width() const { return m_width; }
     uint32_t height() const { return m_height; }
     bool should_close() const { return m_should_close; }
--- engine/sources/core/Window.cc
+++ engine/sources/core/Window.cc
@@ -1,25 +1,34 @@
 #include <vull/core/Window.hh>
 
+#include <vull/support/Array.hh>
 #include <vull/support/Assert.hh>
 #include <vull/vulkan/Context.hh>
 #include <vull/vulkan/Swapchain.hh>
 
 #include <stdlib.h>
+#include <string.h>
 #include <vulkan/vulkan_core.h>
 #include <vulkan/vulkan_xcb.h>
 #include <xcb/xcb_aux.h>
 #include <xcb/xproto.h>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wkeyword-macro"
+#define explicit _explicit
+#include <xcb/xkb.h>
+#undef explicit
+#pragma clang diagnostic pop
+
 namespace vull {
 
-Window::Window(uint32_t width, uint32_t height, bool fullscreen) : m_width(width), m_height(height) {
+Window::Window(uint16_t width, uint16_t height, bool fullscreen) : m_width(width), m_height(height) {
     // Open an X connection.
     m_connection = xcb_connect(nullptr, nullptr);
     VULL_ENSURE(xcb_connection_has_error(m_connection) == 0, "Failed to create X connection");
 
     // Create a window on the first screen and set the title.
     m_id = xcb_generate_id(m_connection);
-    const uint32_t event_mask = 0;
+    const uint32_t event_mask = XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE | XCB_EVENT_MASK_POINTER_MOTION;
     xcb_screen_t *screen = xcb_setup_roots_iterator(xcb_get_setup(m_connection)).data;
     xcb_create_window(m_connection, screen->root_depth, m_id, screen->root, 0, 0, static_cast<uint16_t>(width),
                       static_cast<uint16_t>(height), 0, XCB_WINDOW_CLASS_INPUT_OUTPUT, screen->root_visual,
@@ -44,17 +53,76 @@ Window::Window(uint32_t width, uint32_t height, bool fullscreen) : m_width(width
                             &wm_fullscreen_atom->atom);
         free(wm_fullscreen_atom);
         free(wm_state_atom);
+
+        xcb_pixmap_t cursor_pixmap = xcb_generate_id(m_connection);
+        xcb_create_pixmap(m_connection, 1, cursor_pixmap, m_id, 1, 1);
+
+        const uint32_t cursor_value = xcb_generate_id(m_connection);
+        xcb_create_cursor(m_connection, cursor_value, cursor_pixmap, cursor_pixmap, 0, 0, 0, 0, 0, 0, 0, 0);
+        xcb_free_pixmap(m_connection, cursor_pixmap);
+        xcb_change_window_attributes(m_connection, m_id, XCB_CW_CURSOR, &cursor_value);
     }
+
+    auto use_xkb_request = xcb_xkb_use_extension(m_connection, XCB_XKB_MAJOR_VERSION, XCB_XKB_MINOR_VERSION);
+    free(xcb_xkb_use_extension_reply(m_connection, use_xkb_request, nullptr));
+    map_keycodes();
 
     // Make the window visible and wait for the server to process the requests.
     xcb_map_window(m_connection, m_id);
     xcb_aux_sync(m_connection);
+    xcb_warp_pointer(m_connection, m_id, m_id, 0, 0, m_width, m_height, static_cast<int16_t>(m_width / 2),
+                     static_cast<int16_t>(m_height / 2));
 }
 
 Window::~Window() {
     free(m_delete_window_atom);
     xcb_destroy_window(m_connection, m_id);
     xcb_disconnect(m_connection);
+}
+
+void Window::map_keycodes() {
+    struct KeyPair {
+        const char *name;
+        Key key;
+    };
+    const Array key_pairs{
+        KeyPair{"AD01", Key::Q}, KeyPair{"AD02", Key::W}, KeyPair{"AD03", Key::E},     KeyPair{"AD04", Key::R},
+        KeyPair{"AD05", Key::T}, KeyPair{"AD06", Key::Y}, KeyPair{"AD07", Key::U},     KeyPair{"AD08", Key::I},
+        KeyPair{"AD09", Key::O}, KeyPair{"AD10", Key::P}, KeyPair{"AC01", Key::A},     KeyPair{"AC02", Key::S},
+        KeyPair{"AC03", Key::D}, KeyPair{"AC04", Key::F}, KeyPair{"AC05", Key::G},     KeyPair{"AC06", Key::H},
+        KeyPair{"AC07", Key::J}, KeyPair{"AC08", Key::K}, KeyPair{"AC09", Key::L},     KeyPair{"AB01", Key::Z},
+        KeyPair{"AB02", Key::X}, KeyPair{"AB03", Key::C}, KeyPair{"AB04", Key::V},     KeyPair{"AB05", Key::B},
+        KeyPair{"AB06", Key::N}, KeyPair{"AB07", Key::M}, KeyPair{"LFSH", Key::Shift},
+    };
+
+    auto get_names_request = xcb_xkb_get_names(m_connection, XCB_XKB_ID_USE_CORE_KBD, XCB_XKB_NAME_DETAIL_KEY_NAMES);
+    auto *get_names_reply = xcb_xkb_get_names_reply(m_connection, get_names_request, nullptr);
+    VULL_ENSURE(get_names_reply != nullptr);
+
+    auto *names_value_buffer = xcb_xkb_get_names_value_list(get_names_reply);
+    xcb_xkb_get_names_value_list_t names_value_list;
+    xcb_xkb_get_names_value_list_unpack(names_value_buffer, get_names_reply->nTypes, get_names_reply->indicators,
+                                        get_names_reply->virtualMods, get_names_reply->groupNames,
+                                        get_names_reply->nKeys, get_names_reply->nKeyAliases,
+                                        get_names_reply->nRadioGroups, get_names_reply->which, &names_value_list);
+
+    auto key_name_it = xcb_xkb_get_names_value_list_key_names_iterator(get_names_reply, &names_value_list);
+    for (uint32_t keycode = get_names_reply->minKeyCode; key_name_it.rem > 0; keycode++) {
+        for (const auto &pair : key_pairs) {
+            if (strncmp(static_cast<const char *>(key_name_it.data->name), pair.name, 4) == 0) {
+                m_keycode_map[keycode] = pair.key;
+            }
+        }
+        xcb_xkb_key_name_next(&key_name_it);
+    }
+    free(get_names_reply);
+}
+
+Key Window::translate_keycode(uint8_t keycode) {
+    if (keycode >= m_keycode_map.size()) {
+        return Key::Unknown;
+    }
+    return m_keycode_map[keycode];
 }
 
 Swapchain Window::create_swapchain(const Context &context) {
@@ -73,9 +141,31 @@ void Window::close() {
 }
 
 void Window::poll_events() {
+    m_delta_x = 0;
+    m_delta_y = 0;
     xcb_generic_event_t *event;
     while ((event = xcb_poll_for_event(m_connection)) != nullptr) {
         switch (event->response_type & ~0x80u) {
+        case XCB_KEY_PRESS: {
+            auto *key_press_event = reinterpret_cast<xcb_key_press_event_t *>(event);
+            m_keys[static_cast<uint8_t>(translate_keycode(key_press_event->detail))] = true;
+            break;
+        }
+        case XCB_KEY_RELEASE: {
+            auto *key_release_event = reinterpret_cast<xcb_key_release_event_t *>(event);
+            m_keys[static_cast<uint8_t>(translate_keycode(key_release_event->detail))] = false;
+            break;
+        }
+        case XCB_MOTION_NOTIFY: {
+            auto *motion_event = reinterpret_cast<xcb_motion_notify_event_t *>(event);
+            m_delta_x = motion_event->event_x - static_cast<int16_t>(m_width / 2);  // NOLINT
+            m_delta_y = motion_event->event_y - static_cast<int16_t>(m_height / 2); // NOLINT
+            if (motion_event->event_x != m_width / 2 || motion_event->event_y != m_height / 2) {
+                xcb_warp_pointer(m_connection, m_id, m_id, 0, 0, m_width, m_height, static_cast<int16_t>(m_width / 2),
+                                 static_cast<int16_t>(m_height / 2));
+            }
+            break;
+        }
         case XCB_CLIENT_MESSAGE: {
             const auto &data = reinterpret_cast<xcb_client_message_event_t *>(event)->data;
             if (data.data32[0] == m_delete_window_atom->atom) {
