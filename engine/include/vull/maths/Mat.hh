ffc60c54e69f53fc4c09f143a90a45edb4b904db
Owen Smith <yeeetari@gmail.com>
2022-02-06 10:21:01 +0000

    maths: Create Mat and Vec templates

---
+++ engine/include/vull/maths/Mat.hh
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <vull/maths/Common.hh>
+#include <vull/maths/Vec.hh>
+#include <vull/support/Array.hh>
+
+namespace vull {
+
+template <typename T, unsigned C, unsigned R>
+class Mat {
+    Array<Vec<T, R>, C> m_cols;
+
+public:
+    constexpr Mat() : Mat(T(0)) {}
+    constexpr Mat(T t);
+
+    Vec<T, R> &operator[](unsigned col) { return m_cols[col]; }
+    const Vec<T, R> &operator[](unsigned col) const { return m_cols[col]; }
+};
+
+using Mat3x3f = Mat<float, 3, 3>;
+using Mat4x4f = Mat<float, 4, 4>;
+using Mat3f = Mat3x3f;
+using Mat4f = Mat4x4f;
+
+template <typename T, unsigned C, unsigned R>
+constexpr Mat<T, C, R>::Mat(T t) : m_cols{} {
+    for (unsigned i = 0; i < C; i++) {
+        m_cols[i][i] = t;
+    }
+}
+
+template <typename T>
+Mat<T, 4, 4> look_at(const Vec<T, 3> &camera, const Vec<T, 3> &center, const Vec<T, 3> &up) {
+    const auto f = normalise(center - camera);
+    const auto s = normalise(cross(f, up));
+    const auto u = cross(s, f);
+    Mat<T, 4, 4> ret(T(1));
+    ret[0][0] = s.x();
+    ret[1][0] = s.y();
+    ret[2][0] = s.z();
+    ret[0][1] = u.x();
+    ret[1][1] = u.y();
+    ret[2][1] = u.z();
+    ret[0][2] = -f.x();
+    ret[1][2] = -f.y();
+    ret[2][2] = -f.z();
+    ret[3][0] = -dot(s, camera);
+    ret[3][1] = -dot(u, camera);
+    ret[3][2] = dot(f, camera);
+    return ret;
+}
+
+// Infinite reversed depth.
+// TODO: Take in horizontal_fov?
+template <typename T>
+Mat<T, 4, 4> projection_matrix(T aspect_ratio, T near_plane, T vertical_fov) {
+    const T tan_half_fov = vull::tan(vertical_fov / T(2));
+    Mat<T, 4, 4> ret(T(0));
+    ret[0][0] = T(1) / (aspect_ratio * tan_half_fov);
+    ret[1][1] = T(-1) / tan_half_fov;
+    ret[2][3] = T(-1);
+    ret[3][2] = near_plane;
+    return ret;
+}
+
+} // namespace vull
---
+++ engine/include/vull/maths/Vec.hh
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <vull/maths/Common.hh>
+#include <vull/support/Array.hh>
+
+namespace vull {
+
+#define DEFINE_OP(op, ope)                                                                                             \
+    constexpr Vec operator op(T rhs) const { return Vec(*this) ope rhs; }                                              \
+    constexpr Vec operator op(const Vec &rhs) const { return Vec(*this) ope rhs; }                                     \
+    constexpr Vec &operator ope(T rhs) {                                                                               \
+        for (unsigned i = 0; i < L; i++) {                                                                             \
+            m_elems[i] ope rhs;                                                                                        \
+        }                                                                                                              \
+        return *this;                                                                                                  \
+    }                                                                                                                  \
+    constexpr Vec &operator ope(const Vec &rhs) {                                                                      \
+        for (unsigned i = 0; i < L; i++) {                                                                             \
+            m_elems[i] ope rhs.m_elems[i];                                                                             \
+        }                                                                                                              \
+        return *this;                                                                                                  \
+    }
+
+template <typename T, unsigned L>
+class Vec {
+    Array<T, L> m_elems;
+
+public:
+    constexpr Vec() : Vec(T(0)) {}
+    template <typename... Ts>
+    constexpr Vec(Ts... ts) requires(sizeof...(Ts) == L) : m_elems{ts...} {}
+    constexpr Vec(T t) requires(L != 1);
+
+    DEFINE_OP(+, +=)
+    DEFINE_OP(-, -=)
+    DEFINE_OP(*, *=)
+    DEFINE_OP(/, /=)
+    DEFINE_OP(%, %=)
+    DEFINE_OP(&, &=)
+    DEFINE_OP(|, |=)
+    DEFINE_OP(^, ^=)
+    DEFINE_OP(<<, <<=)
+    DEFINE_OP(>>, >>=)
+
+    constexpr T &operator[](unsigned elem) { return m_elems[elem]; }
+    constexpr T operator[](unsigned elem) const { return m_elems[elem]; }
+    constexpr T x() const requires(L >= 1) { return m_elems[0]; }
+    constexpr T y() const requires(L >= 2) { return m_elems[1]; }
+    constexpr T z() const requires(L >= 3) { return m_elems[2]; }
+    constexpr T w() const requires(L >= 4) { return m_elems[3]; }
+};
+#undef DEFINE_OP
+
+using Vec2f = Vec<float, 2>;
+using Vec3f = Vec<float, 3>;
+using Vec4f = Vec<float, 4>;
+using Vec2u = Vec<uint32_t, 2>;
+using Vec3u = Vec<uint32_t, 3>;
+using Vec4u = Vec<uint32_t, 4>;
+
+template <typename T, unsigned L>
+constexpr Vec<T, L>::Vec(T t) requires(L != 1) {
+    for (unsigned i = 0; i < L; i++) {
+        m_elems[i] = t;
+    }
+}
+
+template <typename T, unsigned L>
+constexpr Vec<T, L> abs(const Vec<T, L> &vec) {
+    Vec<T, L> ret;
+    for (unsigned i = 0; i < L; i++) {
+        ret[i] = abs(vec[i]);
+    }
+    return ret;
+}
+
+template <typename T>
+constexpr Vec<T, 3> cross(const Vec<T, 3> &lhs, const Vec<T, 3> &rhs) {
+    return {
+        lhs.y() * rhs.z() - lhs.z() * rhs.y(),
+        lhs.z() * rhs.x() - lhs.x() * rhs.z(),
+        lhs.x() * rhs.y() - lhs.y() * rhs.x(),
+    };
+}
+
+template <typename T, unsigned L>
+constexpr float distance(const Vec<T, L> &lhs, const Vec<T, L> &rhs) {
+    return magnitude(rhs - lhs);
+}
+
+template <typename T, unsigned L>
+constexpr float dot(const Vec<T, L> &lhs, const Vec<T, L> &rhs) {
+    float ret = 0.0f;
+    for (unsigned i = 0; i < L; i++) {
+        ret += lhs[i] * rhs[i];
+    }
+    return ret;
+}
+
+template <typename T, unsigned L>
+constexpr float magnitude(const Vec<T, L> &vec) {
+    return sqrt(square_magnitude(vec));
+}
+
+template <typename T, unsigned L>
+constexpr Vec<T, L> normalise(const Vec<T, L> &vec) {
+    return vec / magnitude(vec);
+}
+
+template <typename T, unsigned L>
+constexpr float square_magnitude(const Vec<T, L> &vec) {
+    return dot(vec, vec);
+}
+
+} // namespace vull
