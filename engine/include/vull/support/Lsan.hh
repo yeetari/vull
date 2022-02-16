562289d771f0ce4c00497f8b61114c67eabec125
Owen Smith <yeeetari@gmail.com>
2022-02-16 19:33:56 +0000

    support: Cleanup lsan disabler

--- engine/include/vull/support/Lsan.hh
+++ engine/include/vull/support/Lsan.hh
@@ -1,15 +1,19 @@
 #pragma once
 
+#if !defined(__has_feature)
+#define __has_feature(x) 0
+#endif
+
+#if __has_feature(address_sanitizer) || defined(__SANITIZE_ADDRESS__)
+#include <sanitizer/lsan_interface.h>
 namespace vull {
-
-struct LsanDisabler {
-    LsanDisabler();
-    LsanDisabler(const LsanDisabler &) = delete;
-    LsanDisabler(LsanDisabler &&) = delete;
-    ~LsanDisabler();
-
-    LsanDisabler &operator=(const LsanDisabler &) = delete;
-    LsanDisabler &operator=(LsanDisabler &&) = delete;
-};
-
+using LsanDisabler = __lsan::ScopedDisabler;
 } // namespace vull
+#else
+namespace vull {
+struct LsanDisabler {
+    // NOLINTNEXTLINE: Defining the constructor like this suppresses -Wunused-variable
+    LsanDisabler() {}
+};
+} // namespace vull
+#endif
