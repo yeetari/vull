fcd7d77f975f4cc1ab2b0ef701773d3e4fe95be8
Owen Smith <yeeetari@gmail.com>
2022-04-15 14:43:33 +0100

    support: Implement StringView::operator==

--- engine/include/vull/support/StringView.hh
+++ engine/include/vull/support/StringView.hh
@@ -11,6 +11,8 @@ public:
     using Span::Span;
     constexpr StringView(const char *c_string) : Span(c_string, __builtin_strlen(c_string)) {}
 
+    bool operator==(StringView other) const;
+
     constexpr bool empty() const { return length() == 0; }
     constexpr size_t length() const { return size(); }
 };
