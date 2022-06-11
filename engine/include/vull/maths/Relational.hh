882a86b156e1aaa012fe62684f34da35b517f645
Owen Smith <yeeetari@gmail.com>
2022-06-11 20:26:39 +0100

    maths: Add vector relational functions (comparisons, equality)

---
+++ engine/include/vull/maths/Relational.hh
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vull/maths/Vec.hh>
+
+namespace vull {
+
+template <unsigned L>
+bool all(const Vec<bool, L> &vec) {
+    bool ret = true;
+    for (unsigned i = 0; i < L; i++) {
+        ret &= vec[i];
+    }
+    return ret;
+}
+
+template <unsigned L>
+bool any(const Vec<bool, L> &vec) {
+    bool ret = false;
+    for (unsigned i = 0; i < L; i++) {
+        ret |= vec[i];
+    }
+    return ret;
+}
+
+#define DEFINE_CMP(name, op)                                                                                           \
+    template <typename T, unsigned L>                                                                                  \
+    Vec<bool, L> name(const Vec<T, L> &lhs, const Vec<T, L> &rhs) {                                                    \
+        Vec<bool, L> ret;                                                                                              \
+        for (unsigned i = 0; i < L; i++) {                                                                             \
+            ret[i] = lhs[i] op rhs[i];                                                                                 \
+        }                                                                                                              \
+        return ret;                                                                                                    \
+    }
+
+DEFINE_CMP(equal, ==)
+DEFINE_CMP(not_equal, !=)
+DEFINE_CMP(less_than, <)
+DEFINE_CMP(greater_than, >)
+DEFINE_CMP(less_than_equal, <=)
+DEFINE_CMP(greater_than_equal, >=)
+#undef DEFINE_CMP
+
+} // namespace vull
