6f31ecdc3913facda2f5fe816bc73f214e1d9dd2
Owen Smith <yeeetari@gmail.com>
2022-06-15 17:36:40 +0100

    tasklet: Use a semaphore for blocking when no work is available

--- engine/sources/tasklet/Scheduler.cc
+++ engine/sources/tasklet/Scheduler.cc
@@ -1,6 +1,7 @@
 #include <vull/tasklet/Scheduler.hh>
 
 #include <vull/core/Log.hh>
+#include <vull/maths/Common.hh>
 #include <vull/support/Assert.hh>
 #include <vull/support/Atomic.hh>
 #include <vull/support/Optional.hh>
@@ -11,6 +12,7 @@
 #include <vull/tasklet/Tasklet.hh>
 
 #include <pthread.h>
+#include <semaphore.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -19,12 +21,14 @@ namespace vull {
 class Semaphore;
 class TaskletQueue : public WorkStealingQueue<Tasklet> {};
 VULL_GLOBAL(static thread_local TaskletQueue *s_queue = nullptr);
+VULL_GLOBAL(static sem_t s_work_available);
 
 void schedule(Tasklet &&tasklet, Optional<Semaphore &> semaphore) {
     VULL_ASSERT_PEDANTIC(s_queue != nullptr);
     if (semaphore) {
         tasklet.set_semaphore(*semaphore);
     }
+    sem_post(&s_work_available);
     while (!s_queue->enqueue(move(tasklet))) {
         auto queued_tasklet = s_queue->dequeue();
         VULL_ASSERT(queued_tasklet);
@@ -41,8 +45,9 @@ Scheduler::Worker &Scheduler::pick_victim(uint32_t &rng_state) {
 
 Scheduler::Scheduler(uint32_t thread_count) {
     if (thread_count == 0) {
-        thread_count = static_cast<uint32_t>(sysconf(_SC_NPROCESSORS_ONLN)) / 2;
+        thread_count = vull::max(static_cast<uint32_t>(sysconf(_SC_NPROCESSORS_ONLN)) / 2, 2u);
     }
+    sem_init(&s_work_available, 0, 1);
     for (uint32_t i = 0; i < thread_count; i++) {
         auto &worker = m_workers.emplace(new Worker{.scheduler = *this});
         worker->queue = vull::make_unique<TaskletQueue>();
@@ -56,6 +61,7 @@ Scheduler::~Scheduler() {
     for (auto &worker : m_workers) {
         pthread_join(worker->thread, nullptr);
     }
+    sem_destroy(&s_work_available);
 }
 
 bool Scheduler::start(Tasklet &&tasklet) {
@@ -74,6 +80,7 @@ bool Scheduler::start(Tasklet &&tasklet) {
 }
 
 void Scheduler::stop() {
+    sem_post(&s_work_available);
     for (auto &worker : m_workers) {
         worker->running.store(false, MemoryOrder::Relaxed);
     }
@@ -83,20 +90,21 @@ void *Scheduler::thread_loop(void *init_data) {
     auto &[scheduler, queue, _, rng_state, running] = *static_cast<Worker *>(init_data);
     s_queue = queue.ptr();
     while (running.load(MemoryOrder::Relaxed) || !queue->empty()) {
+        sem_wait(&s_work_available);
         if (auto tasklet = queue->dequeue()) {
             tasklet->invoke();
             continue;
         }
         const auto &victim = scheduler.pick_victim(rng_state);
         if (&victim == init_data) {
-            VULL_ASSERT_PEDANTIC(!victim.queue->steal());
+            sem_post(&s_work_available);
             continue;
         }
         if (auto tasklet = victim.queue->steal()) {
             tasklet->invoke();
             continue;
         }
-        usleep(100);
+        sem_post(&s_work_available);
     }
     return nullptr;
 }
