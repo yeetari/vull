3f9504066c2d94bb556e040d090f4c75963ddc0d
Owen Smith <yeeetari@gmail.com>
2022-07-09 17:11:46 +0100

    core: Improve log system

--- engine/sources/core/Log.cc
+++ engine/sources/core/Log.cc
@@ -1,6 +1,7 @@
 #include <vull/core/Log.hh>
 
 #include <vull/support/Array.hh>
+#include <vull/support/Assert.hh>
 #include <vull/support/Atomic.hh>
 #include <vull/support/Optional.hh>
 #include <vull/support/String.hh>
@@ -9,18 +10,20 @@
 #include <vull/support/Vector.hh>
 
 // IWYU pragma: no_include <bits/types/struct_sched_param.h>
+#include <errno.h>
 #include <pthread.h>
 #include <sched.h>
 #include <semaphore.h>
 #include <stdio.h>
+#include <string.h>
 
 namespace vull {
 namespace {
 
 // SPSC lock-free queue. Owning thread is the producer, sink tasklet is the consumer.
+// TODO: Per-thread allocator for log messages?
 class LogQueue {
-    // TODO: Per-thread bump allocator for string data.
-    Array<String, 256> m_slots;
+    Array<Atomic<char *>, 256> m_slots;
     Atomic<int64_t> m_head;
     Atomic<int64_t> m_tail;
 
@@ -33,14 +36,14 @@ void LogQueue::enqueue(String &&message) {
     int64_t head = m_head.load(MemoryOrder::Relaxed);
     int64_t tail = m_tail.load(MemoryOrder::Acquire);
 
-    // Queue is full.
-    if (head - tail >= m_slots.size()) {
+    // TODO: Messages are lost if the queue is full.
+    if (head - tail >= m_slots.size()) [[unlikely]] {
         return;
     }
 
     // Store element in slot and bump the head index.
-    m_slots[static_cast<uint32_t>(head % m_slots.size())] = vull::move(message);
-    m_head.store(head + 1, MemoryOrder::Relaxed);
+    m_slots[static_cast<uint32_t>(head % m_slots.size())].store(message.disown(), MemoryOrder::Relaxed);
+    m_head.store(head + 1, MemoryOrder::Release);
 }
 
 Optional<String> LogQueue::dequeue() {
@@ -51,8 +54,10 @@ Optional<String> LogQueue::dequeue() {
     if (tail >= head) {
         return {};
     }
+
+    char *msg = m_slots[static_cast<uint32_t>(tail % m_slots.size())].exchange(nullptr, MemoryOrder::Relaxed);
     m_tail.store(tail + 1, MemoryOrder::Release);
-    return vull::move(m_slots[static_cast<uint32_t>(tail % m_slots.size())]);
+    return String::move_raw(msg, strlen(msg));
 }
 
 class GlobalState {
@@ -118,6 +123,12 @@ void GlobalState::post() {
 bool GlobalState::wait() {
     pthread_mutex_unlock(&m_queues_mutex);
     if (!m_running.load(MemoryOrder::Relaxed)) {
+        if (sem_trywait(&m_semaphore) == 0) {
+            // Still messages queued.
+            pthread_mutex_lock(&m_queues_mutex);
+            return true;
+        }
+        VULL_ASSERT(errno == EAGAIN);
         return false;
     }
     sem_wait(&m_semaphore);
