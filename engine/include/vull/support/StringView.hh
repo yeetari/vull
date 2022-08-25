3739619f1ab25b37956bc3e44c5409941c6c0e7f
Owen Smith <yeeetari@gmail.com>
2022-08-25 18:09:32 +0100

    support: Declare StringView::operator== inline so it can be constexpr

--- engine/include/vull/support/StringView.hh
+++ engine/include/vull/support/StringView.hh
@@ -11,10 +11,21 @@ public:
     using Span::Span;
     constexpr StringView(const char *c_string) : Span(c_string, __builtin_strlen(c_string)) {}
 
-    bool operator==(StringView other) const;
-
-    constexpr bool empty() const { return length() == 0; }
+    constexpr bool operator==(StringView other) const;
     constexpr size_t length() const { return size(); }
 };
+
+constexpr bool StringView::operator==(StringView other) const {
+    if (data() == nullptr) {
+        return other.data() == nullptr;
+    }
+    if (other.data() == nullptr) {
+        return false;
+    }
+    if (length() != other.length()) {
+        return false;
+    }
+    return __builtin_memcmp(data(), other.data(), length()) == 0;
+}
 
 } // namespace vull
