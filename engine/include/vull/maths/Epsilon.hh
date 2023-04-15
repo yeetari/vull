5dafc957a841ef2039d35ca05f61267c3313f514
Owen Smith <yeeetari@gmail.com>
2023-04-15 20:52:48 +0100

    maths: Add epsilon functions for scalars

--- engine/include/vull/maths/Epsilon.hh
+++ engine/include/vull/maths/Epsilon.hh
@@ -11,24 +11,34 @@ template <>
 inline constexpr float k_fixed_epsilon<float> = 1e-5f;
 
 template <typename T, unsigned L>
-constexpr Vec<bool, L> epsilon_equal(const Vec<T, L> &lhs, const Vec<T, L> &rhs, T epsilon) {
-    return epsilon_equal(lhs, rhs, Vec<T, L>(epsilon));
-}
-
-template <typename T, unsigned L>
 constexpr Vec<bool, L> epsilon_equal(const Vec<T, L> &lhs, const Vec<T, L> &rhs, const Vec<T, L> &epsilon) {
     return less_than_equal(abs(lhs - rhs), epsilon);
 }
 
 template <typename T, unsigned L>
 constexpr bool fuzzy_equal(const Vec<T, L> &lhs, const Vec<T, L> &rhs) {
-    auto epsilon_factor = min(max(abs(lhs), abs(rhs)), Vec<T, L>(T(1)));
+    const auto epsilon_factor = max(abs(lhs), abs(rhs));
     return all(epsilon_equal(lhs, rhs, epsilon_factor * k_fixed_epsilon<T>));
 }
 
 template <typename T, unsigned L>
 constexpr bool fuzzy_zero(const Vec<T, L> &vec) {
-    return all(epsilon_equal(vec, Vec<T, L>(T(0)), k_fixed_epsilon<T>));
+    return all(epsilon_equal(vec, Vec<T, L>(0), Vec<T, L>(k_fixed_epsilon<T>)));
+}
+
+template <typename T>
+constexpr bool epsilon_equal(T lhs, T rhs, T epsilon) {
+    return epsilon_equal(Vec<T, 1>(lhs), Vec<T, 1>(rhs), Vec<T, 1>(epsilon)).x();
+}
+
+template <typename T>
+constexpr bool fuzzy_equal(T lhs, T rhs) {
+    return fuzzy_equal(Vec<T, 1>(lhs), Vec<T, 1>(rhs));
+}
+
+template <typename T>
+constexpr bool fuzzy_zero(T value) {
+    return fuzzy_zero(Vec<T, 1>(value));
 }
 
 } // namespace vull
