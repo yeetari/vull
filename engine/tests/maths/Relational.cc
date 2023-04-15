f67d52b67887374b7b26f56a0b9691965297b710
Owen Smith <yeeetari@gmail.com>
2023-04-15 20:53:42 +0100

    maths: Add some tests for epsilon and relational functions

---
+++ engine/tests/maths/Relational.cc
@@ -0,0 +1,96 @@
+#include <vull/maths/Relational.hh>
+
+#include <vull/maths/Vec.hh>
+#include <vull/support/Assert.hh>
+#include <vull/support/Test.hh>
+
+using namespace vull;
+
+TEST_SUITE(Relational, {
+    ;
+    TEST_CASE(All) {
+        EXPECT(vull::all(Vec<bool, 1>(true)));
+        EXPECT(vull::all(Vec<bool, 2>(true)));
+        EXPECT(!vull::all(Vec<bool, 1>(false)));
+        EXPECT(!vull::all(Vec<bool, 2>(false)));
+        EXPECT(!vull::all(Vec<bool, 2>(false, true)));
+        EXPECT(!vull::all(Vec<bool, 2>(true, false)));
+    }
+
+    TEST_CASE(Any) {
+        EXPECT(vull::any(Vec<bool, 1>(true)));
+        EXPECT(vull::any(Vec<bool, 2>(true)));
+        EXPECT(!vull::any(Vec<bool, 1>(false)));
+        EXPECT(!vull::any(Vec<bool, 2>(false)));
+        EXPECT(vull::any(Vec<bool, 2>(false, true)));
+        EXPECT(vull::any(Vec<bool, 2>(true, false)));
+    }
+
+    TEST_CASE(Equal_Neither) {
+        auto vec = vull::equal(Vec2u(5, 10), Vec2u(15, 20));
+        EXPECT(!vec.x() && !vec.y());
+        EXPECT(!vull::all(vec));
+        EXPECT(!vull::any(vec));
+        EXPECT(vull::all(vull::equal(vec, Vec<bool, 2>(false, false))));
+    }
+
+    TEST_CASE(Equal_One) {
+        auto vec = vull::equal(Vec2u(5, 10), Vec2u(5, 15));
+        EXPECT(vec.x() && !vec.y());
+        EXPECT(!vull::all(vec));
+        EXPECT(vull::any(vec));
+        EXPECT(vull::all(vull::equal(vec, Vec<bool, 2>(true, false))));
+    }
+
+    TEST_CASE(Equal_Both) {
+        auto vec = vull::equal(Vec2u(5, 5), Vec2u(5, 5));
+        EXPECT(vec.x() && vec.y());
+        EXPECT(vull::all(vec));
+        EXPECT(vull::any(vec));
+        EXPECT(vull::all(vull::equal(vec, Vec<bool, 2>(true, true))));
+    }
+
+    TEST_CASE(NotEqual_Neither) {
+        auto vec = vull::not_equal(Vec2u(5, 5), Vec2u(5, 5));
+        EXPECT(!vec.x() && !vec.y());
+        EXPECT(!vull::all(vec));
+        EXPECT(!vull::any(vec));
+        EXPECT(vull::all(vull::not_equal(vec, Vec<bool, 2>(true, true))));
+    }
+
+    TEST_CASE(NotEqual_One) {
+        auto vec = vull::not_equal(Vec2u(5, 10), Vec2u(5, 15));
+        EXPECT(!vec.x() && vec.y());
+        EXPECT(!vull::all(vec));
+        EXPECT(vull::any(vec));
+        EXPECT(vull::all(vull::not_equal(vec, Vec<bool, 2>(true, false))));
+    }
+
+    TEST_CASE(NotEqual_Both) {
+        auto vec = vull::not_equal(Vec2u(5, 10), Vec2u(15, 20));
+        EXPECT(vec.x() && vec.y());
+        EXPECT(vull::all(vec));
+        EXPECT(vull::any(vec));
+        EXPECT(vull::all(vull::not_equal(vec, Vec<bool, 2>(false, false))));
+    }
+
+    TEST_CASE(LessThan) {
+        auto vec = vull::less_than(Vec3u(5, 10, 15), Vec3u(4, 10, 16));
+        EXPECT(!vec.x() && !vec.y() && vec.z());
+    }
+
+    TEST_CASE(GreaterThan) {
+        auto vec = vull::greater_than(Vec3u(5, 10, 15), Vec3u(4, 10, 16));
+        EXPECT(vec.x() && !vec.y() && !vec.z());
+    }
+
+    TEST_CASE(LessThanEqual) {
+        auto vec = vull::less_than_equal(Vec3u(5, 10, 15), Vec3u(4, 10, 16));
+        EXPECT(!vec.x() && vec.y() && vec.z());
+    }
+
+    TEST_CASE(GreaterThanEqual) {
+        auto vec = vull::greater_than_equal(Vec3u(5, 10, 15), Vec3u(4, 10, 16));
+        EXPECT(vec.x() && vec.y() && !vec.z());
+    }
+})
