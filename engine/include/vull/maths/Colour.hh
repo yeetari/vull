6684475ad74b3c4b6d6158c0b47da4e94c8bf21e
Owen Smith <yeeetari@gmail.com>
2023-05-30 18:15:30 +0100

    maths: Make colour spaces explicit and assume HSL is in sRGB

--- engine/include/vull/maths/Colour.hh
+++ engine/include/vull/maths/Colour.hh
@@ -5,18 +5,61 @@
 
 namespace vull {
 
+/// A class for representing a colour in linear space.
 class Colour {
     Vec4f m_rgba;
 
     explicit Colour(const Vec4f &rgba) : m_rgba(rgba) {}
 
 public:
-    static Colour from_hsl(float h, float s, float l);
-    static Colour from_hsla(float h, float s, float l, float a);
-    static Colour from_rgb(float r, float g, float b);
-    static Colour from_rgba(float r, float g, float b, float a);
-    static Colour from_rgb(const Vec3f &rgb);
-    static Colour from_rgba(const Vec4f &rgba);
+    /**
+     * Creates a colour from RGB(A) components in linear space.
+     *
+     * @param r red
+     * @param g green
+     * @param b blue
+     * @param a alpha
+     * @return  a Colour in linear space
+     * @note    alpha is passed through unmodified
+     */
+    static Colour from_rgb(float r, float g, float b, float a = 1.0f) { return Colour(Vec4f(r, g, b, a)); }
+
+    /**
+     * @copybrief  from_rgb(float, float, float, float)
+     * @param rgba RGBA components in Vec4f form
+     * @return     a Colour in linear space
+     */
+    static Colour from_rgb(const Vec4f &rgba) { return Colour(rgba); }
+
+    /**
+     * Creates a colour from RGB(A) components in the sRGB colour space.
+     *
+     * @param r red
+     * @param g green
+     * @param b blue
+     * @param a alpha
+     * @return  a Colour in linear space
+     * @note    alpha is passed through unmodified
+     */
+    static Colour from_srgb(float r, float g, float b, float a = 1.0f);
+
+    /**
+     * Creates a colour from HSL(A) components, assumed to be in the sRGB colour space.
+     *
+     * @param h hue
+     * @param s saturation
+     * @param l lightness
+     * @param a alpha
+     * @return  a Colour in linear space
+     * @note    alpha is passed through unmodified
+     */
+    static Colour from_hsl(float h, float s, float l, float a = 1.0f);
+
+    /**
+     * Generates a random colour.
+     *
+     * @return a Colour in linear space
+     */
     static Colour make_random();
 
     static Colour black() { return from_rgb(0.0f, 0.0f, 0.0f); }
@@ -24,13 +67,13 @@ public:
 
     Colour() = default;
 
-    Vec3f to_rgb() const { return m_rgba; }
-    Vec4f to_rgba() const { return m_rgba; }
+    Vec3f rgb() const { return m_rgba; }
+    Vec4f rgba() const { return m_rgba; }
 };
 
 template <>
 inline Colour lerp(Colour a, Colour b, float x) {
-    return Colour::from_rgba(vull::lerp(a.to_rgba(), b.to_rgba(), x));
+    return Colour::from_rgb(vull::lerp(a.rgba(), b.rgba(), x));
 }
 
 } // namespace vull
--- engine/sources/maths/Colour.cc
+++ engine/sources/maths/Colour.cc
@@ -1,15 +1,27 @@
 #include <vull/maths/Colour.hh>
 
 #include <vull/maths/Random.hh>
+#include <vull/maths/Relational.hh>
 #include <vull/maths/Vec.hh>
 
 namespace vull {
+namespace {
 
-Colour Colour::from_hsl(float h, float s, float l) {
-    return from_hsla(h, s, l, 1.0f);
+// See the Khronos Data Format Specification v1.3.1 section 13.3.1
+Vec3f srgb_to_linear(Vec3f srgb) {
+    auto r1 = srgb / 12.92f;
+    auto r2 = vull::pow((srgb + 0.055f) / 1.055f, Vec3f(2.4f));
+    return vull::select(r1, r2, vull::greater_than(srgb, Vec3f(0.04045f)));
 }
 
-static float hue_to_rgb(float p, float q, float t) {
+// See the Khronos Data Format Specification v1.3.1 section 13.3.2
+[[maybe_unused]] Vec3f linear_to_srgb(Vec3f linear) {
+    auto r1 = linear * 12.92f;
+    auto r2 = vull::pow(linear, Vec3f(1.0f / 2.4f)) * 1.055f - 0.055f;
+    return vull::select(r1, r2, vull::greater_than(linear, Vec3f(0.0031308f)));
+}
+
+float hue_to_rgb(float p, float q, float t) {
     if (t < 0.0f) {
         t += 1.0f;
     }
@@ -28,32 +40,22 @@ static float hue_to_rgb(float p, float q, float t) {
     return p;
 }
 
-Colour Colour::from_hsla(float h, float s, float l, float a) {
+} // namespace
+
+Colour Colour::from_srgb(float r, float g, float b, float a) {
+    return Colour(Vec4f(srgb_to_linear(Vec3f(r, g, b)), a));
+}
+
+Colour Colour::from_hsl(float h, float s, float l, float a) {
     if (s == 0.0f) {
-        return from_rgba(l, l, l, a);
+        return from_rgb(l, l, l, a);
     }
     float q = l < 0.5f ? l * (1.0f + s) : l + s - l * s;
     float p = 2.0f * l - q;
     float r = hue_to_rgb(p, q, h + 1.0f / 3.0f);
     float g = hue_to_rgb(p, q, h);
     float b = hue_to_rgb(p, q, h - 1.0f / 3.0f);
-    return from_rgba(r, g, b, a);
-}
-
-Colour Colour::from_rgb(float r, float g, float b) {
-    return from_rgba(r, g, b, 1.0f);
-}
-
-Colour Colour::from_rgba(float r, float g, float b, float a) {
-    return Colour(Vec4f(r, g, b, a));
-}
-
-Colour Colour::from_rgb(const Vec3f &rgb) {
-    return from_rgba(Vec4f(rgb, 1.0f));
-}
-
-Colour Colour::from_rgba(const Vec4f &rgba) {
-    return Colour(rgba);
+    return from_srgb(r, g, b, a);
 }
 
 Colour Colour::make_random() {
