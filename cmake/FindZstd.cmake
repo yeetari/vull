8851773746e3f5ae1755ea19a6ff54f379a8d831
Owen Smith <yeeetari@gmail.com>
2023-06-30 18:59:45 +0100

    cmake: Format all files

--- cmake/FindZstd.cmake
+++ cmake/FindZstd.cmake
@@ -29,58 +29,58 @@ include(FindPackageHandleStandardArgs)
 
 find_package(PkgConfig QUIET)
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_Zstd QUIET libzstd)
-  if(PC_Zstd_FOUND)
-    set(Zstd_VERSION_STRING ${PC_Zstd_VERSION})
-  endif()
+    pkg_check_modules(PC_Zstd QUIET libzstd)
+    if(PC_Zstd_FOUND)
+        set(Zstd_VERSION_STRING ${PC_Zstd_VERSION})
+    endif()
 endif()
 
 find_path(Zstd_INCLUDE_DIR
-  NAMES zstd.h
-  HINTS ${PC_Zstd_INCLUDE_DIRS})
+    NAMES zstd.h
+    HINTS ${PC_Zstd_INCLUDE_DIRS})
 mark_as_advanced(Zstd_INCLUDE_DIR)
 
 if(NOT Zstd_LIBRARY)
-  find_library(Zstd_LIBRARY
-    NAMES zstd
-    HINTS ${PC_Zstd_LIBRARY_DIRS})
-  mark_as_advanced(Zstd_LIBRARY)
+    find_library(Zstd_LIBRARY
+        NAMES zstd
+        HINTS ${PC_Zstd_LIBRARY_DIRS})
+    mark_as_advanced(Zstd_LIBRARY)
 endif()
 
 if(Zstd_INCLUDE_DIR AND NOT Zstd_VERSION_STRING)
-  if(EXISTS "${Zstd_INCLUDE_DIR}/zstd.h")
-    foreach(_Zstd_version_component MAJOR MINOR RELEASE)
-      file(STRINGS
-	"${Zstd_INCLUDE_DIR}/zstd.h"
-	Zstd_${_zstd_version_component}_str
-	REGEX "^#define ZSTD_VERSION_${_Zstd_version_component}")
-      string(REGEX REPLACE "^#define ZSTD_VERSION_${_Zstd_version_component}.+([0-9]+)" "\\1" Zstd_${_Zstd_version_component} "${Zstd_${_zstd_version_component}_str}")
-      unset(Zstd_${_zstd_version_component}_str)
-    endforeach()
-    set(Zstd_VERSION_STRING "${Zstd_MAJOR}.${Zstd_MINOR}.${Zstd_RELEASE}")
-    unset(Zstd_MAJOR)
-    unset(Zstd_MINOR)
-    unset(Zstd_RELEASE)
-  endif()
+    if(EXISTS "${Zstd_INCLUDE_DIR}/zstd.h")
+        foreach(_Zstd_version_component MAJOR MINOR RELEASE)
+            file(STRINGS
+                "${Zstd_INCLUDE_DIR}/zstd.h"
+                Zstd_${_zstd_version_component}_str
+                REGEX "^#define ZSTD_VERSION_${_Zstd_version_component}")
+            string(REGEX REPLACE "^#define ZSTD_VERSION_${_Zstd_version_component}.+([0-9]+)" "\\1" Zstd_${_Zstd_version_component} "${Zstd_${_zstd_version_component}_str}")
+            unset(Zstd_${_zstd_version_component}_str)
+        endforeach()
+        set(Zstd_VERSION_STRING "${Zstd_MAJOR}.${Zstd_MINOR}.${Zstd_RELEASE}")
+        unset(Zstd_MAJOR)
+        unset(Zstd_MINOR)
+        unset(Zstd_RELEASE)
+    endif()
 endif()
 
 find_package_handle_standard_args(Zstd
-  REQUIRED_VARS Zstd_LIBRARY Zstd_INCLUDE_DIR
-  VERSION_VAR Zstd_VERSION_STRING)
+    REQUIRED_VARS Zstd_LIBRARY Zstd_INCLUDE_DIR
+    VERSION_VAR Zstd_VERSION_STRING)
 
 if(Zstd_FOUND)
-  set(Zstd_LIBRARIES ${Zstd_LIBRARY})
-  set(Zstd_INCLUDE_DIRS ${Zstd_INCLUDE_DIR})
+    set(Zstd_LIBRARIES ${Zstd_LIBRARY})
+    set(Zstd_INCLUDE_DIRS ${Zstd_INCLUDE_DIR})
 
-  if(NOT TARGET Zstd::Zstd)
-    add_library(Zstd::Zstd UNKNOWN IMPORTED)
-    set_target_properties(Zstd::Zstd PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${Zstd_INCLUDE_DIRS}")
+    if(NOT TARGET Zstd::Zstd)
+        add_library(Zstd::Zstd UNKNOWN IMPORTED)
+        set_target_properties(Zstd::Zstd PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${Zstd_INCLUDE_DIRS}")
 
-    if(EXISTS "${Zstd_LIBRARY}")
-      set_target_properties(Zstd::Zstd PROPERTIES
-	IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-	IMPORTED_LOCATION "${Zstd_LIBRARY}")
+        if(EXISTS "${Zstd_LIBRARY}")
+            set_target_properties(Zstd::Zstd PROPERTIES
+                IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                IMPORTED_LOCATION "${Zstd_LIBRARY}")
+        endif()
     endif()
-  endif()
 endif()
--- cmake/VullAddTests.cmake
+++ cmake/VullAddTests.cmake
@@ -1,10 +1,10 @@
 get_filename_component(pwd "${TEST_EXECUTABLE}" DIRECTORY)
 
 execute_process(COMMAND "${TEST_EXECUTABLE}" --list-tests
-        WORKING_DIR "${pwd}"
-        OUTPUT_VARIABLE list_stdout
-        ERROR_VARIABLE list_stderr
-        RESULT_VARIABLE list_result)
+    WORKING_DIR "${pwd}"
+    OUTPUT_VARIABLE list_stdout
+    ERROR_VARIABLE list_stderr
+    RESULT_VARIABLE list_result)
 
 if(NOT ${list_result} EQUAL 0)
     message(FATAL_ERROR "Failed to discover tests: ${list_stdout}${list_stderr}")
