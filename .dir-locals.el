7ce7f8f71e2873ab6917a2ebb1f5d825268fea21
Owen Smith <owen@owensmith.uk>
2024-07-22 21:41:20 +0100

    meta: Add emacs dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((fill-column . 120)
+         (indent-tabs-mode . nil)))
+ (c++-mode . ((c-basic-offset . 4)
+              (c-file-offsets . ((arglist-intro . ++)
+                                 (innamespace . 0)
+                                 (member-init-intro . 0))))))
