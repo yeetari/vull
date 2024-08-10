7b7d41292aaca0d9c4944c3d4be8e547d13791cc
Matthew Smith <matthew@matthew.as>
2024-08-10 13:24:23 +0000

    build: Add VULL_ENABLE_COVERAGE to instrument build

---
+++ cmake/FindGcov.cmake
@@ -0,0 +1,60 @@
+#[=======================================================================[.rst:
+FindGcov
+--------
+
+Find the gcov tool.
+
+IMPORTED Targets
+^^^^^^^^^^^^^^^^
+
+This module defines :prop_tgt:`IMPORTED` target ``Gcov::gcov``, if
+gcov has been found.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module defines the following variables:
+
+``Gcov_FOUND``
+  True if gcov tool was found.
+``Gcov_EXECUTABLE``
+  Path to where the gcov executable is located.
+``Gcov_VERSION_STRING``
+  The version of the gcov tool, for example "14.2.0".
+
+Functions
+^^^^^^^^^
+
+.. command:: gcov_append_compiler_flags
+#]=======================================================================]
+include(CheckCXXCompilerFlag)
+include(FindPackageHandleStandardArgs)
+
+find_program(Gcov_EXECUTABLE NAMES gcov)
+mark_as_advanced(Gcov_EXECUTABLE)
+
+if(Gcov_EXECUTABLE AND NOT Gcov_VERSION)
+  execute_process(COMMAND "${Gcov_EXECUTABLE}" --version
+    OUTPUT_VARIABLE Gcov_VERSION_STRING)
+  string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+" Gcov_VERSION "${Gcov_VERSION_STRING}")
+  unset(Gcov_VERSION_STRING)
+endif()
+
+find_package_handle_standard_args(Gcov
+  REQUIRED_VARS Gcov_EXECUTABLE
+  VERSION_VAR Gcov_VERSION)
+
+if(Gcov_FOUND AND NOT TARGET Gcov::gcov)
+  add_executable(Gcov::gcov IMPORTED)
+  set_target_properties(Gcov::gcov PROPERTIES
+    IMPORTED_LOCATION "${Gcov_EXECUTABLE}")
+endif()
+
+function(gcov_append_compiler_flags)
+  check_cxx_compiler_flag("-fprofile-abs-path" HAVE_cxx_profile_abs_path)
+
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage" PARENT_SCOPE)
+  if(HAVE_cxx_profile_abs_path)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage -fprofile-abs-path" PARENT_SCOPE)
+  endif()
+endfunction()
---
+++ cmake/FindGcovr.cmake
@@ -0,0 +1,59 @@
+#[=======================================================================[.rst:
+FindGcovr
+--------
+
+Find the gcovr tool.
+
+IMPORTED Targets
+^^^^^^^^^^^^^^^^
+
+This module defines :prop_tgt:`IMPORTED` target ``Gcovr::gcovr``, if
+gcovr has been found.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module defines the following variables:
+
+``Gcovr_FOUND``
+  True if gcovr tool was found.
+``Gcovr_EXECUTABLE``
+  Path to where the gcovr executable is located.
+``Gcovr_VERSION_STRING``
+  The version of the gcovr tool, for example "7.2".
+
+Functions
+^^^^^^^^^
+
+.. command:: gcovr_add_report_target
+#]=======================================================================]
+include(FindPackageHandleStandardArgs)
+
+find_program(Gcovr_EXECUTABLE NAMES gcovr)
+mark_as_advanced(Gcovr_EXECUTABLE)
+
+if(Gcovr_EXECUTABLE AND NOT Gcovr_VERSION)
+  execute_process(COMMAND "${Gcovr_EXECUTABLE}" --version
+    OUTPUT_VARIABLE Gcovr_VERSION_STRING)
+  string(REGEX MATCH "[0-9]+\\.[0-9]+" Gcovr_VERSION "${Gcovr_VERSION_STRING}")
+  unset(Gcovr_VERSION_STRING)
+endif()
+
+find_package_handle_standard_args(Gcovr
+  REQUIRED_VARS Gcovr_EXECUTABLE
+  VERSION_VAR Gcovr_VERSION)
+
+if(Gcovr_FOUND AND NOT TARGET Gcovr::gcovr)
+  add_executable(Gcovr::gcovr IMPORTED)
+  set_target_properties(Gcovr::gcovr PROPERTIES
+    IMPORTED_LOCATION "${Gcovr_EXECUTABLE}")
+endif()
+
+function(gcovr_add_report_target)
+  set(oneValueArgs DESTINATION FORMAT)
+  set(multiValueArgs "")
+  cmake_parse_arguments(ARGS "" "${oneValueArgs}" "" ${ARGN})
+  if(ARGS_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "Unknown keywords: \"${ARGS_UNPARSED_ARGUMENTS}\"")
+  endif()
+endfunction()
