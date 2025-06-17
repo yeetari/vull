aa7810ee8bf6e1e07549f8ce3d8b393da254ee22
Owen Smith <owen@owensmith.uk>
2025-06-17 23:43:03 +0100

    tasklet: Add Promise

---
+++ engine/include/vull/tasklet/promise.hh
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <vull/support/atomic.hh>
+#include <vull/support/utility.hh>
+
+#include <stdint.h>
+
+namespace vull {
+
+template <typename>
+class SharedPtr;
+class Tasklet;
+
+class PromiseBase {
+    Atomic<Tasklet *> m_wait_list;
+
+protected:
+    void wake_all();
+
+public:
+    bool add_waiter(Tasklet *tasklet);
+    bool is_fulfilled() const;
+    void wait();
+};
+
+template <typename T>
+class Promise : public PromiseBase {
+    AlignedStorage<T> m_storage{};
+
+public:
+    Promise() = default;
+    Promise(const Promise &) = delete;
+    Promise(Promise &&) = delete;
+    ~Promise();
+
+    Promise &operator=(const Promise &) = delete;
+    Promise &operator=(Promise &&) = delete;
+
+    void fulfill(const T &value);
+    void fulfill(T &&value);
+
+    T &value() { return m_storage.get(); }
+    const T &value() const { return m_storage.get(); }
+};
+
+template <>
+class Promise<void> : public PromiseBase {
+public:
+    void fulfill() { wake_all(); }
+};
+
+template <typename T>
+class SharedPromise final : public Promise<T> {
+    friend SharedPtr<SharedPromise<T>>;
+
+private:
+    mutable Atomic<uint32_t> m_ref_count{0};
+    void add_ref() const;
+    void sub_ref() const;
+};
+
+template <typename T>
+Promise<T>::~Promise() {
+    if constexpr (!vull::is_trivially_destructible<T>) {
+        if (is_fulfilled()) {
+            m_storage.release();
+        }
+    }
+}
+
+template <typename T>
+void Promise<T>::fulfill(const T &value) {
+    m_storage.set(value);
+    wake_all();
+}
+
+template <typename T>
+void Promise<T>::fulfill(T &&value) {
+    m_storage.set(vull::move(value));
+    wake_all();
+}
+
+template <typename T>
+void SharedPromise<T>::add_ref() const {
+    m_ref_count.fetch_add(1, vull::memory_order_relaxed);
+}
+
+template <typename T>
+void SharedPromise<T>::sub_ref() const {
+    if (m_ref_count.fetch_sub(1, vull::memory_order_acq_rel) == 1) {
+        delete this;
+    }
+}
+
+} // namespace vull
---
+++ engine/sources/tasklet/promise.cc
@@ -0,0 +1,51 @@
+#include <vull/tasklet/promise.hh>
+
+#include <vull/support/atomic.hh>
+#include <vull/support/utility.hh>
+#include <vull/tasklet/tasklet.hh>
+
+namespace vull {
+
+#define FULFILLED_SENTINEL vull::bit_cast<Tasklet *>(-1ull)
+
+void PromiseBase::wake_all() {
+    // Atomically swap list with the fulfilled sentinel.
+    auto *tasklet = m_wait_list.exchange(FULFILLED_SENTINEL, vull::memory_order_acquire);
+    while (tasklet != nullptr) {
+        // Dequeue from the list before rescheduling.
+        auto *next = tasklet->pop_linked_tasklet();
+
+        // The tasklet may still be in the middle of yielding.
+        // TODO: Find a way to avoid this.
+        while (tasklet->state() == TaskletState::Running) {
+        }
+
+        // Reschedule the tasklet.
+        vull::schedule(vull::exchange(tasklet, next));
+    }
+}
+
+bool PromiseBase::add_waiter(Tasklet *tasklet) {
+    auto *head = m_wait_list.load(vull::memory_order_relaxed);
+    do {
+        if (head == FULFILLED_SENTINEL) {
+            // Promise was fulfilled. Make sure to clear the linked tasklet.
+            tasklet->set_linked_tasklet(nullptr);
+            return false;
+        }
+        tasklet->set_linked_tasklet(head);
+    } while (!m_wait_list.compare_exchange_weak(head, tasklet, vull::memory_order_release));
+    return true;
+}
+
+bool PromiseBase::is_fulfilled() const {
+    return m_wait_list.load(vull::memory_order_relaxed) == FULFILLED_SENTINEL;
+}
+
+void PromiseBase::wait() {
+    if (add_waiter(Tasklet::current())) {
+        vull::yield();
+    }
+}
+
+} // namespace vull
