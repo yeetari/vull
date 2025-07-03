76eed2579fa6fba617477ce1cb257f6a5b03ff27
Owen Smith <owen@owensmith.uk>
2025-07-03 13:05:15 +0100

    cmake: Cleanup inconsistent indentation

--- cmake/FindGcovr.cmake
+++ cmake/FindGcovr.cmake
@@ -33,27 +33,27 @@ find_program(Gcovr_EXECUTABLE NAMES gcovr)
 mark_as_advanced(Gcovr_EXECUTABLE)
 
 if(Gcovr_EXECUTABLE AND NOT Gcovr_VERSION)
-  execute_process(COMMAND "${Gcovr_EXECUTABLE}" --version
-    OUTPUT_VARIABLE Gcovr_VERSION_STRING)
-  string(REGEX MATCH "[0-9]+\\.[0-9]+" Gcovr_VERSION "${Gcovr_VERSION_STRING}")
-  unset(Gcovr_VERSION_STRING)
+    execute_process(COMMAND "${Gcovr_EXECUTABLE}" --version
+        OUTPUT_VARIABLE Gcovr_VERSION_STRING)
+    string(REGEX MATCH "[0-9]+\\.[0-9]+" Gcovr_VERSION "${Gcovr_VERSION_STRING}")
+    unset(Gcovr_VERSION_STRING)
 endif()
 
 find_package_handle_standard_args(Gcovr
-  REQUIRED_VARS Gcovr_EXECUTABLE
-  VERSION_VAR Gcovr_VERSION)
+    REQUIRED_VARS Gcovr_EXECUTABLE
+    VERSION_VAR Gcovr_VERSION)
 
 if(Gcovr_FOUND AND NOT TARGET Gcovr::gcovr)
-  add_executable(Gcovr::gcovr IMPORTED)
-  set_target_properties(Gcovr::gcovr PROPERTIES
-    IMPORTED_LOCATION "${Gcovr_EXECUTABLE}")
+    add_executable(Gcovr::gcovr IMPORTED)
+    set_target_properties(Gcovr::gcovr PROPERTIES
+        IMPORTED_LOCATION "${Gcovr_EXECUTABLE}")
 endif()
 
 function(gcovr_add_report_target)
-  set(oneValueArgs DESTINATION FORMAT)
-  set(multiValueArgs "")
-  cmake_parse_arguments(ARGS "" "${oneValueArgs}" "" ${ARGN})
-  if(ARGS_UNPARSED_ARGUMENTS)
-    message(FATAL_ERROR "Unknown keywords: \"${ARGS_UNPARSED_ARGUMENTS}\"")
-  endif()
+    set(oneValueArgs DESTINATION FORMAT)
+    set(multiValueArgs "")
+    cmake_parse_arguments(ARGS "" "${oneValueArgs}" "" ${ARGN})
+    if(ARGS_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unknown keywords: \"${ARGS_UNPARSED_ARGUMENTS}\"")
+    endif()
 endfunction()
